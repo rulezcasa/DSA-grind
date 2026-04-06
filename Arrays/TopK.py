cd60c81df09a2b42e5889eee1222e9f7fd47c322
Harish <harish.thangaraj03@outlook.com>
2026-04-06 09:14:09 +0530

    topk frequencies

--- Arrays/TopK.py
+++ Arrays/TopK.py
@@ -3,9 +3,16 @@ TOP K FREQUENT ELEMENTS
 
 - Given an integer array and an integer k
 - return the k most frequent elements
+- For eg : Input: nums = [1,1,1,2,2,3], k = 2 and Output: [1,2]
 '''
 
-''
+'''
+APPROACH
+
+- Using hashmaps and sorted() function :
+    1. 
+'''
+
 
 from typing import List
 
@@ -17,10 +24,28 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
         else:
             hashmap[num]=1
 
-    print(sorted(list(hashmap.keys()), reverse=True))
+    hashmap_sorted=sorted(hashmap.items(), key=lambda item: item[1], reverse=True) #sorted() uses (nlogn) worst case time complexity, could be better.
 
-    for i 
-    return []
+    '''
+    - hashmap.items() -> converts into list of tuples (key,value)
+    - sorted() -> by default sorts it based on the first element of the tuple(key) but since we want to sort by values :
+        - key=lambda item: item[1] ->  lamdba is just an anonymous function when called returns item[1] which is nothing but the value
+        - reverse=True since we want it in descending order (highest to lowest)
+    '''
+
+    '''Working of lambda:
+    - when sorted on X, each element of X is passed to key argument
+    - So we define the key argument as a function that returns item[1] i.e the value
+    '''
+
+    #print(hashmap_sorted)
+
+    topk=[item[0] for item in hashmap_sorted[:k]] #Pull out just the keys (item[0]) from the items of hashmap_sorted top k
+    print(topk)
+
+topKFrequent([4,1,-1,2,-1,2,3],2)
+
+
 
 
         
